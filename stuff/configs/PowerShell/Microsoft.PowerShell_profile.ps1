f9317b6b060bbd5c3d749a52185d5484131686f5
travino <120686325+travino@users.noreply.github.com>
2026-01-29 13:45:33 +0100

    Add files via upload

---
+++ stuff/configs/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,32 @@
+using namespace System.Management.Automation
+using namespace System.Management.Automation.Language
+function prompt {
+    $color = Get-Random -Min 1 -Max 16
+    Write-Host ("PS " + $(Get-Location) +">") -NoNewLine -ForegroundColor $Color
+    return " "
+}
+Register-ArgumentCompleter -Native -CommandName winget -ScriptBlock {
+    param($wordToComplete, $commandAst, $cursorPosition)
+        [Console]::InputEncoding = [Console]::OutputEncoding = $OutputEncoding = [System.Text.Utf8Encoding]::new()
+        $Local:word = $wordToComplete.Replace('"', '""')
+        $Local:ast = $commandAst.ToString().Replace('"', '""')
+        winget complete --word="$Local:word" --commandline "$Local:ast" --position $cursorPosition | ForEach-Object {
+            [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+        }
+}
+Import-Module PSCompletions
+$PSReadLineOptions = @{
+    PredictionSource = "HistoryAndPlugin"
+    HistorySearchCursorMovesToEnd = $true
+    TerminateOrphanedConsoleApps = $true
+}
+Set-PSReadLineOption @PSReadLineOptions
+Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
+Set-PSReadLineKeyHandler -Chord 'Tab' -Function MenuComplete
+Set-PSReadLineKeyHandler -Chord 'Ctrl+Spacebar' -Function Complete
+Set-PSReadLineKeyHandler -Chord 'Ctrl+RightArrow' -Function ForwardWord
+Set-PSReadLineKeyHandler -Chord 'Ctrl+Backspace' -Function BackwardDeleteWord
+Set-PSReadLineKeyHandler -Chord 'Shift+RightArrow' -Function AcceptNextSuggestionWord
+Invoke-Expression -Command $(gh completion -s powershell | Out-String)
+#fnm env --use-on-cd | Out-String | Invoke-Expression

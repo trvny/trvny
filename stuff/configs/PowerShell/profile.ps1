58cf7652c27995226934e1c1b2009364f87e9db5
trvny <120686325+trvny@users.noreply.github.com>
2026-08-02 19:23:47 +0200

    Delete stuff/configs/PowerShell directory

--- stuff/configs/PowerShell/profile.ps1
+++
@@ -1,8 +0,0 @@
-[Net.ServicePointManager]::SecurityProtocol =
-    [Net.ServicePointManager]::SecurityProtocol -bor
-    [Net.SecurityProtocolType]::Tls12
-$OutputEncoding = [System.Text.UTF8Encoding]::new()
-try {
-    [Console]::OutputEncoding = $OutputEncoding
-    [Console]::InputEncoding  = $OutputEncoding
-} catch { }   # brak realnej konsoli - przekierowane stdin/stdout

2a102d8dfcd856bcf4e05d84b11b4bfd0e98d2fb
trvny <120686325+trvny@users.noreply.github.com>
2026-09-05 16:34:05 +0200

    cleanup: retire migrated Paint.NET ICO source

--- stuff/other/paintdotnet-ico/modern/IcoFileTypeFactoryModern.cs
+++
@@ -1,9 +0,0 @@
-using PaintDotNet.FileTypes;
-
-namespace Travny.PaintDotNetIco;
-
-public sealed class IcoFileTypeFactoryModern : IFileTypeFactory
-{
-    public IFileType[] CreateFileTypes(IFileTypeHost host) =>
-        new IFileType[] { new IcoFileTypeModern(host) };
-}
